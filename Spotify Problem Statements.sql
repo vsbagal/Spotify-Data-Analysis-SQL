1f1ac4decb3ab2d137cb16db886459eb49f7afbb
Vishal Bagal <125578339+vsbagal@users.noreply.github.com>
2023-04-20 22:34:50 +0530

    Update and rename Spotify Problem Statements.sql to Spotify Problem Statements

--- Spotify Problem Statements.sql
+++
@@ -1,8 +0,0 @@
-
--- Questions to be solved
-
--- QUE1. Who is the senior most employee based on job title?
-
--- QUE2. Which countries have the most Invoices?
-
--- QUE3. What are the top 3 values of the total invoice?
